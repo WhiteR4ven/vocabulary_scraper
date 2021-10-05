b25a74f11bbfdfc4b775f10fe954fb262ed72c79
WhiteR4ven <72654470+WhiteR4ven@users.noreply.github.com>
2021-10-05 12:21:53 +0200

    Initial Commit
    Description of the raw functions

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#packages to install:
+# pip install soupsieve
+# pip install beautifulsoup4
+# pip install bs4
+# pip install dnspython
+# pip install pymongo
+
+import requests  # lib to make web requests
+from bs4 import BeautifulSoup
+
+# read in url-list
+
+# loop through vocab input
+  
+# download content from the web url
+
+# scrape the online data
+      
+# save Data to output.txt
+    
+# EOF
